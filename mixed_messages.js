f6c5d42e777eaef6a61ad893524226e49d313513
Abigail Crimlis <abby.crimlis@outlook.com>
2021-02-21 09:41:28 +0000

    removed notes

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,6 +1,4 @@
-//this project is a portfolio peice that solidifies my knowledge i have learnt so far.
-//the mixed message app uses JS and node.js to output a random string of messages when the user runs the program. 
-//the topic i have chosen is mood brightener texts. the program will consist of 3 different data banks.
+
 
 const databank = {
     group1: [
